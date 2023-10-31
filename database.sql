471adecb8f9ef3a11099e927b0f8ddc7b107332d
Tho Pham (Alex) <20041501+thopd88@users.noreply.github.com>
2023-10-31 17:19:36 +0700

    Modify database to link books to authors

--- database.sql
+++ database.sql
@@ -1,10 +1,23 @@
 CREATE DATABASE `bookproject`
 
+-- book_new.authors definition
+
+CREATE TABLE `authors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- book_new.books definition
+
 CREATE TABLE `books` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `category` varchar(100) DEFAULT NULL,
   `year` int DEFAULT NULL,
-  `publisher` varchar(50) DEFAULT NULL,
-  `author` varchar(100) DEFAULT NULL,
+  `publisher` varchar(100) DEFAULT NULL,
+  `author_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-);
+  KEY `books_FK` (`author_id`),
+  CONSTRAINT `books_FK` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- readme.md
+++ readme.md
@@ -1,3 +1,31 @@
+Modify database to link books to authors
+--
+```
+-- book_new.authors definition
+
+CREATE TABLE `authors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+```
+
+```
+-- book_new.books definition
+
+CREATE TABLE `books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `publisher` varchar(100) DEFAULT NULL,
+  `author_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `books_FK` (`author_id`),
+  CONSTRAINT `books_FK` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+```
+
 Steps to create database
 --
 - Create a database named bookproject
