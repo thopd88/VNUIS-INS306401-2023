bf9383ae5c7e92bb578848ad82de802cf16f8671
Tho Pham (Alex) <20041501+thopd88@users.noreply.github.com>
2023-11-21 17:57:28 +0700

    Demo create and show book info

--- BookManagement/routes/web.php
+++ BookManagement/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Book::create([
+        'title' => 'The Great Gatsby',
+        'author' => 'F. Scott Fitzgerald',
+        'publisher' => 'Charles Scribner\'s Sons'
+    ]);
+    return Book::all();
 });
